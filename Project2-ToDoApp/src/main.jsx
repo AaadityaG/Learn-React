dfec6c7a67de2120ae47268bd8f3aa4ade500d08
Aditya <helloadig@gmail.com>
2023-10-06 14:16:11 +0530

    Form and React Todo App Project

--- Project2-ToDoApp/src/main.jsx
+++ Project2-ToDoApp/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './Evs.jsx'
-// import Makeform from './Forms.jsx' 
-import Makeform from './Forms.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    hii
-    <Makeform />
+    <App />
   </React.StrictMode>,
 )
--- TopicEvents/src/Forms.jsx
+++ TopicEvents/src/Forms.jsx
@@ -1,24 +1,79 @@
-import { useCallback, useState } from "react";
-import "./Forms.css"
+import { useState } from "react";
+import "./Form.css"
 
 function Makeform(){
-    const [addName, setName] = useState("Sir");
-    let greetTo = addName;
-    const inputEvent = (e) => {
-        greetTo = e.target.value;
-    }
-    const handleClick = () =>{
-        setName(greetTo);
-    }
-    return(
-        <>
+    const [fullData, setfName] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        phone: "",
+    });
+
+    const inputEventF = (e) => {
+        const {name, value} = e.target; 
+        setfName((prev) => {
+            console.log(prev);
+            return {
+                ...prev,
+                [name]: value,
+            };
+
+            // if(name==="fname"){
+            //     return{
+            //         fname:value,
+            //         lname: prev.lname,
+            //         email: prev.email,
+            //         phone: prev.phone,
+            //     };
+            // }
+            // if(name === "lname"){
+            //     return {
+            //         fname: prev.fname,
+            //         lname: value,
+            //         email: prev.email,
+            //         phone: prev.phone,
+            //     }
+            // }
+            // if(name === "email"){
+            //     return {
+            //         fname: prev.fname,
+            //         lname: prev.lname,
+            //         email: value,
+            //         phone: prev.phone,
+            //     }
+            // }
+            // if(name === "phone"){
+            //     return {
+            //         fname: prev.fname,
+            //         lname: prev.lname,
+            //         email: prev.email,
+            //         phone: value,
+            //     }
+            // }
+        });
+        
+        
+        const onSub = (e) =>{
+            e.preventDefault();
+        }
+
+        return(
+            <>
             <div>
-                <h1 style={{color:"#333"}}>Hello, {greetTo}</h1>
-                <input type="text" placeholder="Enter Your Name" onChange={inputEvent}/>
-                <button onClick={handleClick}>Click me👍</button>
+                <form onSubmit={onSub}>
+                    <h1 style={{color:"#333"}}>Hello, {fullData.fname} {fullData.lname}</h1>
+                    <h>{fullData.email}</h>
+                    <h3>{fullData.phone}</h3>
+                    <input autoComplete="off" type="text" name="fname" placeholder="Enter Your First Name" value={fullData.fname} onChange={inputEventF}/>
+                    <input autoComplete="off" type="text" name="lname" placeholder="Enter Your Last Name" value={fullData.lname} onChange={inputEventF}/>
+                    <input autoComplete="off" type="email" name="email" placeholder="Enter Your Email" value={fullData.email} onChange={inputEventF}/>
+                    <input autoComplete="off" type="number" name="phone" placeholder="Enter Your Phone Number" value={fullData.phone} onChange={inputEventF}/>
+                    {/* <button type="submit">Click me👍</button> */}
+                </form>
             </div>
-        </>
-    );
+            </>
+        );
+    }
 }
 
 export default Makeform;
